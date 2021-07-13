354019cff268e41cf1b95256da0e83f7a58806c4
Michael Neeley <micneeley14@gmail.com>
2021-07-13 10:53:49 -0400

    adds getevents query

--- apollos-church-api/src/data/Event.js
+++ apollos-church-api/src/data/Event.js
@@ -19,8 +19,41 @@ export const resolver = {
 };
 
 export class dataSource extends Event.dataSource {
-  getEvents = ({ limit = 10, ministry }) =>
-    this.findRecent()
-      .top(limit)
+  getEvents = async ({ ministry }) => {
+    if (!ministry) return this.findRecent().get();
+    // search through available ministries
+    const audiences = await this.request('DefinedValues')
+      .filter('DefinedTypeId eq 99')
       .get();
+    const { guid } = audiences.find((audience) => audience.value === ministry);
+    if (!guid) return [];
+
+    // get all events that match the Guid on the Ministries attribute
+    const values = await this.request('AttributeValues')
+      .filter(`AttributeId eq 5694 and Value eq '${guid}'`)
+      .get();
+    const eventCalendarItems = await Promise.all(
+      values.map(async ({ entityId }) =>
+        this.request('EventCalendarItems')
+          .find(entityId)
+          .get()
+      )
+    );
+    const eventItems = await Promise.all(
+      eventCalendarItems.map(async ({ eventItemId }) =>
+        this.request('EventItems')
+          .find(eventItemId)
+          .get()
+      )
+    );
+    const occurrances = await Promise.all(
+      eventItems.map(async ({ id }) =>
+        this.request()
+          .filter(`EventItemId eq ${id}`)
+          .expand('Schedule')
+          .get()
+      )
+    );
+    return occurrances.flat();
+  };
 }
