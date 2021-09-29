30a04d8631e666549406054a9b898d99a9c0d695
nlewis84 <nathan@differential.com>
2021-09-29 10:42:35 -0500

    added channels to config.yml and new algo for weekly scripture

--- apollos-church-api/src/data/ActionAlgorithms.js
+++ apollos-church-api/src/data/ActionAlgorithms.js
@@ -4,6 +4,7 @@ class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
     PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
+    DAILY_SCRIPTURE_FEED: this.dailyScriptureFeedAlgorithm.bind(this),
   };
 
   async contentFeedAlgorithm({
@@ -48,6 +49,36 @@ class dataSource extends ActionAlgorithm.dataSource {
           .top(limit)
           .skip(skip)
           .sort([{ field: 'Priority', direction: 'asc' }])
+          .get()
+      )
+    )).flat();
+
+    return items.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: item.contentChannel?.name,
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
+  async dailyScriptureFeedAlgorithm({
+    category = '',
+    channelIds = [],
+    limit = 20,
+    skip = 0,
+  } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = (await Promise.all(
+      channelIds.map(async (channel) =>
+        (await ContentItem.byContentChannelId(channel, category))
+          // First, get all the items for this week (even if not live yet)
+          .top(limit)
+          .skip(skip)
+          // Then, sort chronologically ascending
           .get()
       )
     )).flat();
