c872a9506e0b3052eb8872cacd61ba66f41a060a
nlewis84 <nathan@differential.com>
2021-10-01 14:12:29 -0500

    made the daily scripture a content feed

--- apollos-church-api/src/data/ActionAlgorithms.js
+++ apollos-church-api/src/data/ActionAlgorithms.js
@@ -5,7 +5,6 @@ class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
     PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
-    DAILY_SCRIPTURE_FEED: this.dailyScriptureFeedAlgorithm.bind(this),
     WEEKLY_SCRIPTURE_FEED: this.weeklyScriptureFeedAlgorithm.bind(this),
   };
 
@@ -51,34 +50,6 @@ class dataSource extends ActionAlgorithm.dataSource {
           .top(limit)
           .skip(skip)
           .sort([{ field: 'Priority', direction: 'asc' }])
-          .get()
-      )
-    )).flat();
-
-    return items.map((item, i) => ({
-      id: `${item.id}${i}`,
-      title: item.title,
-      subtitle: item.contentChannel?.name,
-      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
-      image: ContentItem.getCoverImage(item),
-      action: 'READ_CONTENT',
-      summary: ContentItem.createSummary(item),
-    }));
-  }
-
-  async dailyScriptureFeedAlgorithm({
-    category = '',
-    channelIds = [],
-    limit = 20,
-    skip = 0,
-  } = {}) {
-    const { ContentItem } = this.context.dataSources;
-
-    const items = (await Promise.all(
-      channelIds.map(async (channel) =>
-        (await ContentItem.byContentChannelId(channel, category))
-          .top(limit)
-          .skip(skip)
           .get()
       )
     )).flat();
