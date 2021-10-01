22daf4202ab01b1f91e3ea8a85ff88050a7d7b7c
nlewis84 <nathan@differential.com>
2021-10-01 12:49:39 -0500

    set start of week to Monday and added algo for daily scripture

--- apollos-church-api/src/data/ActionAlgorithms.js
+++ apollos-church-api/src/data/ActionAlgorithms.js
@@ -5,6 +5,7 @@ class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
     PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
+    DAILY_SCRIPTURE_FEED: this.dailyScriptureFeedAlgorithm.bind(this),
     WEEKLY_SCRIPTURE_FEED: this.weeklyScriptureFeedAlgorithm.bind(this),
   };
 
@@ -65,6 +66,34 @@ class dataSource extends ActionAlgorithm.dataSource {
     }));
   }
 
+  async dailyScriptureFeedAlgorithm({
+    category = '',
+    channelIds = [],
+    limit = 20,
+    skip = 0,
+  } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = (await Promise.all(
+      channelIds.map(async (channel) =>
+        (await ContentItem.byContentChannelId(channel, category))
+          .top(limit)
+          .skip(skip)
+          .get()
+      )
+    )).flat();
+
+    return items.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: item.contentChannel?.name,
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
   async weeklyScriptureFeedAlgorithm({
     category = '',
     channelIds = [],
@@ -85,7 +114,7 @@ class dataSource extends ActionAlgorithm.dataSource {
     )).flat();
 
     const itemsByDate = items.filter((key) =>
-      isThisWeek(new Date(key.startDateTime))
+      isThisWeek(new Date(key.startDateTime), 1)
     );
 
     return itemsByDate.map((item, i) => ({
